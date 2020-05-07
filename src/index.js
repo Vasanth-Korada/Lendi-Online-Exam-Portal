0dfc58d2e5eff6139024287d67b0e1d470216f09
srikanthmudili <srikanth512000@gmail.com>
2020-05-07 17:10:33 +0530

    login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NotFound from './NotFound';
 import {Route, BrowserRouter as Router} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <Router>
     <Route exact path="/" component={App}/>
+    <Route exact path="/error" component={NotFound}/>
   </Router>,
   document.getElementById('root')
 );
